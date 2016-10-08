2a170545b1d0d7f760a1dde272a5521d9ba85c70
Toomas Koost <toomas@wizibe.com>
2016-10-08 14:36:36 +0300

    html ja css creation

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,19 @@
+import java.io.*;
 public class HelloWorld {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         System.out.println("Hello World!");
+
+
+        //kirjutab index faili valmis
+        PrintWriter index = new PrintWriter("index.html", "UTF-8");
+        index.println("<h1>SUUR PEALKIRI BOLDIS</h1>");
+        index.println("Teema");
+        index.close();
+
+        //kirjutab styles.css faili valmis
+        PrintWriter css = new PrintWriter("styles.css", "UTF-8");
+        css.println("h1 {color: red; text-align: center; font-size: 72px;}");
+        css.println("The second line");
+        css.close();
     }
 }
